eff7bcf99f6615e79d1f5ee34688f0343276f50a
MazinAli0928 <mazinali09@outlook.com>
2024-10-30 09:53:23 +0530

    Update a.c

--- a.c
+++ a.c
@@ -1,1 +1,23 @@
-vzvchg
+git --version
+git init
+git clone "Repo address"
+file creation
+ls
+git add .
+git commit -m "Message
+git remote add origin "repo address"
+git remote set-url origin "https://access token@repolink"
+git push origin main
+git branch branchname
+git branch
+git status
+git checkout branchname
+file creation
+git add .
+git commit -m "message"
+git push origin branchname
+git checkout main
+git commit -m "message"
+git merge branchname
+git push origin main
+
